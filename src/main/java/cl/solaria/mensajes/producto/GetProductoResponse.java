35d89332b07a425c482941fa500f1327d102ef74
Pedro Carrasco <pedrodonte@gmail.com>
2019-08-16 12:49:41 -0400

    clean

--- src/main/java/cl/solaria/mensajes/producto/GetProductoResponse.java
+++ src/main/java/cl/solaria/mensajes/producto/GetProductoResponse.java
@@ -2,7 +2,7 @@
 // Este archivo ha sido generado por la arquitectura JavaTM para la implantación de la referencia de enlace (JAXB) XML v2.2.7 
 // Visite <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a> 
 // Todas las modificaciones realizadas en este archivo se perderán si se vuelve a compilar el esquema de origen. 
-// Generado el: 2019.08.09 a las 11:27:41 AM CLT 
+// Generado el: 2019.08.09 a las 03:03:58 PM CLT 
 //
 
 
